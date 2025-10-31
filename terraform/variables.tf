971e77ad61ac5965ea091330fbe5d3ccb55f7596
adnannazir944-svg <adnannazir944@gmail.com>
2025-10-31 01:34:25 -0400

    Fix: add defaults and disable input for CI

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,20 @@
-variable "region" { type = string }
-variable "name_prefix" { type = string }
-variable "cidr_vpc" { type = string }
-variable "cidr_public_subnet" { type = string }
+variable "region" { 
+type = string
+default = "us-east-1"
+
+ }
+variable "name_prefix" { 
+type = string 
+default = "jr2025"
+}
+variable "cidr_vpc" { 
+type = string 
+default = "10.0.0.0/16"
+}
+variable "cidr_public_subnet" { 
+type = string 
+default = "10.0.1.0/24"
+}
 variable "instance_type" {
   type    = string
   default = "t2.micro"
